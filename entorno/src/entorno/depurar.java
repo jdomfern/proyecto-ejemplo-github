6c157487a7a930ce5ed8d69fd18c5609a6c98a8d
khaler.012 <khaler.012@pc6-11.41009330.41.andared.ced.junta-andalucia.es>
2022-11-08 21:42:53 +0100

    cambiar numero
    el 9 se cambia por 8

--- entorno/src/entorno/depurar.java
+++ entorno/src/entorno/depurar.java
@@ -4,14 +4,16 @@ public class depurar {
 
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
-		int b=9;
+		int b=8;
 		int c=0;
-		int d=7;
+		int e=7;
 		
-		d++;
+		e++;
+		b=b+e;
 		c=b/c;
 		
 		System.out.println(c);
+
 
 	}
 
