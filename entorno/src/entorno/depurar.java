69a1a7a4015e09e465dcef2d5d594a4a690a31fb
khaler.012 <khaler.012@pc6-11.41009330.41.andared.ced.junta-andalucia.es>
2022-11-08 21:36:45 +0100

    Cambio de numero
    El 8 se ha cambiado por el 9

---
+++ entorno/src/entorno/depurar.java
@@ -0,0 +1,18 @@
+package entorno;
+
+public class depurar {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+		int b=9;
+		int c=0;
+		int d=7;
+		
+		d++;
+		c=b/c;
+		
+		System.out.println(c);
+
+	}
+
+}
